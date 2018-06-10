3bf9d9100ce5f7799c9ae98950bff35f8495e743
elertan <denkievits@gmail.com>
2018-06-10 16:28:50 +0200

    Almost fully working titlebar, also added Open Sans font

--- src/components/Titlebar.tsx
+++ src/components/Titlebar.tsx
@@ -1,3 +1,4 @@
+import { ipcRenderer } from 'electron';
 import * as React from "react";
 
 const styles = {
@@ -8,13 +9,49 @@ const styles = {
     justifyContent: "center",
     width: "100%",
   } as React.CSSProperties,
+  logoContainer: {
+    paddingLeft: 15,
+    paddingRight: 25,
+  } as React.CSSProperties,
+  logo: {
+    marginTop: 15,
+    fontSize: 18,
+    fontFamily: 'LeagueFont',
+  } as React.CSSProperties,
+  titlebarActionsContainer: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    padding: 15,
+  } as React.CSSProperties,
 };
 
 class Titlebar extends React.Component<{}, {}> {
   public render() {
     return (
       <div style={styles.container}>
-        Test
+        {/* Logo */}
+        <div className="drag-region" style={styles.logoContainer}>
+          <p style={styles.logo}>SkinKK</p>
+        </div>
+        {/* NavBar */}
+        <div>
+          NavBar
+        </div>
+        {/* Empty Drag Space */}
+        <div className="drag-region" style={{flex: 1}} />
+        {/* Titlebar Actions */}
+        <div style={styles.titlebarActionsContainer}>
+            <i
+              className="fas fa-minus minimize-button"
+              style={{ marginRight: 15, position: 'relative', top: 3 }}
+              onClick={() => ipcRenderer.send('Titlebar_minimize')}
+            />
+            <i
+              className="fas fa-window-close close-button"
+              onClick={() => ipcRenderer.send('Titlebar_close')}
+            />
+        </div>
       </div>
     );
   }
