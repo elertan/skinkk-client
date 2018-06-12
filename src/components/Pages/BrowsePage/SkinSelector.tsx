8bab055fef1ffca973f127602db1250718e88850
Dennis Kievits <denkievits@gmail.com>
2018-06-12 21:47:40 +0200

    Finally finished logic for the skin selector

--- src/components/Pages/BrowsePage/SkinSelector.tsx
+++ src/components/Pages/BrowsePage/SkinSelector.tsx
@@ -30,17 +30,44 @@ const styles = {
   } as React.CSSProperties,
 };
 
+const maxSwitchSkins = 5;
+
 class SkinSelector extends React.Component<IProps, {}> {
   public render() {
     const champion = this.props.champion;
     const skin = this.props.selectedSkin || this.props.champion.skins[0];
+    const skinIndex = champion.skins.indexOf(skin);
+    const maxSkins = maxSwitchSkins > champion.skins.length ? champion.skins.length : maxSwitchSkins;
+    const skinBorderAmount = Math.floor(maxSkins / 2);
+
+    let switchSkins = [skin];
+    for (let i = 1; i < maxSkins; i++) {
+      const isPrepending = i > skinBorderAmount;
+      if (!isPrepending) {
+        if (skinIndex + i > champion.skins.length - 1) {
+          switchSkins = [...switchSkins, champion.skins[i]];
+          continue;
+        }
+        switchSkins = [...switchSkins, champion.skins[skinIndex + i]];
+        continue;
+      }
+
+      const backwardsIndex = skinIndex - i;
+      if (backwardsIndex < 0) {
+        switchSkins = [champion.skins[champion.skins.length + skinBorderAmount - i], ...switchSkins];
+        continue;
+      }
+      switchSkins = [champion.skins[skinIndex - Math.abs(backwardsIndex)], ...switchSkins];
+    }
     return (
       <div style={styles.container}>
         <div style={styles.currentSkinContainer}>
           {this.getImageDiv(champion.key, skin.num)}
         </div>
         <div style={styles.switchContainer}>
-
+          {switchSkins.map((s: Skin, i: number) =>
+          <div>{s.name}</div>,
+          )}
         </div>
       </div>
     );
